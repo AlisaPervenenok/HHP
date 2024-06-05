48e93927120614bf16485413b70c9b92d0705811
Bzelga <kogdanadomilonoegonet@mail.ru>
2024-06-05 11:52:24 +0500

    Общение в чатике только с ботом

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from pywebio.session import run_async, run_js
 import asyncio
 
 chat_msgs = []
-online_users = set()
 MAX_MESSAGE_COUNT = 100
 
 
@@ -21,21 +20,6 @@ async def main():
     msg_box = output()
     put_scrollable(msg_box, height=300, keep_bottom=True)
 
-    nickname = await input("Войти в чат", required=True, placeholder="Ваше имя", validate=lambda n: "Такой ник уже "
-                                                                                                    "используется!"
-    if n in online_users or n == 'Ку-ку' else None)
-    online_users.add(nickname)
-    # человек
-    chat_msgs.append(("📢  ", f"{nickname} присоединился к чату!"))
-    msg_box.append(put_markdown(f"`{nickname}` присоединился к чату!"))
-    # bot
-    botname = "bot from " + nickname
-    online_users.add(botname)
-    chat_msgs.append(("📢 ", f" {botname} присоединился к чату!"))
-    msg_box.append(put_markdown(f" `{botname}` присоединился к чату!"))
-
-    refresh_task = run_async(refresh_msg(nickname, msg_box))
-
     while True:
         data = await input_group("Новое сообщение:", [
             input(placeholder="Текст сообщения", name="msg"),
@@ -46,24 +30,16 @@ async def main():
         if data is None:
             break
         # человек
-        msg_box.append(put_markdown(f"`{nickname}:` {data['msg']}"))
-        chat_msgs.append((nickname, data['msg']))
+        msg_box.append(put_markdown(f"`{'Name'}:` {data['msg']}"))
+
         # bot
         # chat_msgs.append((botname, data['msg'])) #необходим для теста эхо
         #get_topic_task = run_async(get_topic(botname, msg_box, data['msg']))
         #get_search_task = run_async(get_search(botname, msg_box, data['msg']))
-        get_weather_task = run_async(get_weather(botname, msg_box, data['msg']))
+        get_weather_task = run_async(get_weather("botname", msg_box, data['msg']))
 
-    #выход из чата
-    refresh_task.close()
-    #get_topic_task.close()
-    #get_search_task.close()
     get_weather_task.close()
-    online_users.remove(nickname)
-    online_users.remove(botname)
     toast("Вы вышли из чата!")
-    msg_box.append(put_markdown(f"⚠️ Пользователь `{nickname}` покинул чат!"))
-    chat_msgs.append(("⚠️ ", f"Пользователь {nickname} покинул чат!"))
 
     put_buttons(["Перезайти"], onclick=lambda btn: run_js('window.location.reload()'))
 
@@ -106,9 +82,9 @@ async def get_weather(botname1, msg_box, search_str):
     try:
         res = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=61.241778&lon=73.393032&appid=6545f8fa9439776a69436d7d3acb4010&units=metric")
         data = res.json()
-        chat_msgs.append((f"{botname1}", f" возможно вы искали погоду: \" {data.get('name')}:  {data.get('main').get('temp')} °C \" "))
+        msg_box.append(put_markdown(f"`{botname1}`: возможно вы искали погоду: \" {data.get('name')}:  {data.get('main').get('temp')} °C \" "))
     except Exception as e:
-        chat_msgs.append((f"{botname1}", f" погоду не нашёл, и вот в чём причина: \" {e} \" "))
+        msg_box.append(put_markdown(f"`{botname1}`: погоду не нашёл, и вот в чём причина: \" {e} \" "))
         pass
 
 if __name__ == "__main__":
